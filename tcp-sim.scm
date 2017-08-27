395ac1304480984e15bd4400ea2172987d9bc208
Tomoyuki Sahara <sahara@caddr.net>
2017-08-27 23:37:13 +0900

    wip

--- tcp-sim.scm
+++ tcp-sim.scm
@@ -13,17 +13,20 @@
 ;;
 ;; Future Events
 ;;
-(define *events: (make-binary-heap :key car))
+(define *events* (make-binary-heap :key car))
 
 (define (event-add time name . args)
+  (binary-heap-push! *events* (cons* time name args)))
 
-  )
-
-
+(define (events-print)
+  (let1 h (binary-heap-copy *events*)
+    (while (not (binary-heap-empty? h))
+      (print (binary-heap-pop-min! h)))))
 
 (define (send-syn from to)
-
+  (event-add rtt :syn '())
   )
 
 
 (send-syn client server)
+(events-print)
