0a8ebb9bc8c247ec5e4ab911c749c1b9983efea5
Tomoyuki Sahara <sahara@caddr.net>
2020-01-16 23:21:45 +0900

    hkdf-expand

--- quic.scm
+++ quic.scm
@@ -16,7 +16,14 @@
 
 (define (hash-symbol->class sym)
   (case sym
+    ((:sha1)   <sha1>)
     ((:sha256) <sha256>)))
+
+(define (hash-symbol->bytelength sym)
+  (case sym
+    ((:sha1)   20)
+    ((:sha256) 32)))
+
 
 ;; https://tools.ietf.org/html/rfc5869#section-2.2
 (define (hkdf-extract hash salt ikm)
@@ -26,6 +33,26 @@
     (hmac-update! hmac ikm)
     (string->u8vector (hmac-final! hmac))))
 
+;; https://tools.ietf.org/html/rfc5869#section-2.3
+(define (hkdf-expand hash prk info l)
+  (let ((hasher  (hash-symbol->class hash))
+        (hashlen (hash-symbol->bytelength hash))
+        (prk-str (u8vector->string prk))
+        (okm     (make-u8vector l)))
+    (let loop ((n 1)
+               (T ""))
+      (let1 hmac (make <hmac> :key prk-str :hasher hasher)
+        (hmac-update! hmac T)
+        (hmac-update! hmac info)
+        (hmac-update! hmac (bytevector n))
+        (let1 digest (string->u8vector (hmac-final! hmac))
+          (if (<= l (* hashlen n))
+              (u8vector-copy! okm (* hashlen (- n 1))
+                              digest 0 (- l (* hashlen (- n 1))))
+              (begin
+                (u8vector-copy! okm (* hashlen (- n 1)) digest 0 hashlen)
+                (loop (+ n 1) digest))))))
+    okm))
 
 (define (open-quic receiver)
   (let1 sock (make-socket AF_INET SOCK_DGRAM)
